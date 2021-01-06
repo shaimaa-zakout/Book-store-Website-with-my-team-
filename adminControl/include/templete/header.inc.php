61e239d39d32d8f714ba9b7b14db736a179dcdc8
shaimaa-zakout <63237476+shaimaa-zakout@users.noreply.github.com>
2021-01-06 16:55:17 +0200

    Create header.inc.php

---
+++ adminControl/include/templete/header.inc.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Store Book</title>
+        <link rel="stylesheet" type="text/css" href="layout/css/bootstrap.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--===============================================================================================-->	
+        <link rel="icon" type="image/png" href="layout/img/icons/favicon.ico"/>
+        <!--===============================================================================================-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="<?php echo $font; ?>font-awesome-4.7.0/css/font-awesome.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="<?php echo $ven; ?>animate/animate.css">
+        <!--===============================================================================================-->	
+        <link rel="stylesheet" type="text/css" href="<?php echo $ven; ?>css-hamburgers/hamburgers.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="<?php echo $ven; ?>select2/select2.min.css">
+        <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href=
+              "<?php echo $css; ?>util.css">
+
+        <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>main.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>style2.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $css; ?>myMain.css">
+
+
+
+    </head>
+
+    <body>
