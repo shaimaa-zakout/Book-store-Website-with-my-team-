58d2044152aa29fdd0446f8d54f2eb7a80f91bf7
shaimaa-zakout <63237476+shaimaa-zakout@users.noreply.github.com>
2021-01-06 16:55:11 +0200

    Create footer.inc.php

---
+++ adminControl/include/templete/footer.inc.php
@@ -0,0 +1,10 @@
+<footer>
+
+</footer>
+
+<script type="text/javascript" src="layout/js/jquery"></script>
+
+<script type="text/javascript" src="layout/js/backend.js"></script>
+<script type="text/javascript" src="layout/js/bootstrap.min.js"></script>
+</body>
+</html>
